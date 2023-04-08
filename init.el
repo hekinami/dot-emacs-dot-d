0d3f9cc4f4e63c4891726f2335403a3e30892749
Zou Bibo <zoubibo@amiunique.net>
2023-04-08 19:45:29 +0800

    basic config for straight/use-package

---
+++ init.el
@@ -0,0 +1,14 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://ghproxy.com/https://raw.githubusercontent.com/hekinami/straight.el/master/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
